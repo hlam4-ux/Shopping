d4fb5dcddf4067d3dc46f91c21488173834abbab
A Student <student@nowhere.edu>
2026-04-28 02:57:45 +0000

    Apr 27, 2026, 7:57 PM

--- main.cpp
+++ main.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
+/**
+ * this function calculates the shipping cost
+ * method tells which shipping type we use
+ * cost is the price of the order
+ * it returns the shipping cost or -1 if doing wrong
+ */
+double getShippingCost(int method, double cost){
+   double result = -1;
+   
+   //Drone Delivery
+   if (method == 1) {
+      if (cost < 100) {
+         result = -1;
+      } else if (cost < 500) {
+         result = 50;
+      } else {
+         result = cost * 0.1;
+      }
+   }
 
+   //2-Day Delivery
+   if (method == 2) {
+      if (cost <= 300) {
+         result = 10 + cost * 0.02;
+      } else {
+         result = 0;
+      }
+   }
+
+   //Standard Delivery
+   if (method == 3) {
+      if (cost < 35) {
+         result = 5;
+      } else if (cost < 100) {
+         result = 8;
+      }else {
+         result = 0;
+      }
+   }
+      return result;
+}
 int main()
 {
   cout << "Hello world" << endl;
