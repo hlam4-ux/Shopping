4cf5b886e760ca88ac91cb8c7f7711804c135f15
A Student <student@nowhere.edu>
2026-04-28 02:52:56 +0000

    Apr 27, 2026, 7:52 PM

--- tests.cpp
+++ tests.cpp
@@ -24,7 +24,7 @@ using namespace std;
  * this function calculates the shipping cost
  * method tells which shipping type we use
  * cost is the price of the order
- * 
+ * it returns 
  * 
  * 
  */
